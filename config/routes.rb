c1e2b8ec2a09afddbf6bf47c2c030a8398bb74cb
Thomas Casady <tscasady@gmail.com>
2023-01-03 17:03:28 -0600

    feat: Displays merchants item names on merchant item index page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :customers
-  resources :merchants
+  resources :merchants do
+    resources :items, only: [:index], controller: "merchants/items"
+  end
   get '/merchants/:id/dashboard', to: "merchants#show"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
