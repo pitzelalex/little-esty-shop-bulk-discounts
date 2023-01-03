0edc7ac76214f13cfb0b73974a2e497b20dce8d6
Kerynn Davis <kerynn.davis@gmail.com>
2023-01-03 15:47:15 -0800

    feat: Add admin merchant index page with merchant names

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   resources :customers
   resources :merchants
   get '/merchants/:id/dashboard', to: "merchants#show"
+  namespace :admin do
+    resources :merchants, only: [:index]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
