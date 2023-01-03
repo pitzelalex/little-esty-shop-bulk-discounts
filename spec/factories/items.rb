befccf321b28a04a9de0b388f6a4ed400aacd8ce
Thomas Casady <tscasady@gmail.com>
2023-01-03 16:51:34 -0600

    test: Add test for displaying names of items to merchant items index page

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :item do
-    sequence(:name) { |n| "item #{n}" }
+    sequence(:name) { |n| "Item_#{n}" }
     sequence(:description) { "Default Description" }
     sequence(:unit_price) { |n| 10000 + (n * 1000)}
     association :merchant
