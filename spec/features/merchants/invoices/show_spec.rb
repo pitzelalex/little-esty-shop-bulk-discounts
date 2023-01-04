139fdf3015ff192fbfd002fef338d35f4b84f086
Alex Pitzel <pitzelalex@gmail.com>
2023-01-04 14:01:49 -0500

    test: Create merchant invoices view test

---
+++ spec/features/merchants/invoices/show_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe 'The merchant invocie show page', type: :feature do
+  let(:merchant_1) { create(:merchant_with_invoices) }
+
+  describe "I visit one of my merchant's invoice show pages" do
+    it "Displays information about that invoice" do
+      merchant_1.invoices.each do |invoice|
+        visit merchant_invoices_path(merchant_1, invoice)
+
+        require 'pry'; binding.pry
+        expect(page).to have_content("Id: #{invoice.id}")
+        expect(page).to have_content("Status: #{invoice.status}")
+        expect(page).to have_content("Created at: #{invoice.created_at.strftime("%A, %B %e, %Y")}")
+        expect(page).to have_content("Customer: #{invoice.customer.first_name} #{invoice.customer.last_name}")
+      end
+    end
+  end
+end
