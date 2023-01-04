7c2c2734eb60174e7e3cf92e909dc9962fb3cb7b
Kerynn Davis <kerynn.davis@gmail.com>
2023-01-03 17:39:26 -0800

    feat: Add link for each merchant name to admin merchant show page

--- spec/features/admins/merchants/index_spec.rb
+++ spec/features/admins/merchants/index_spec.rb
@@ -13,5 +13,17 @@ RSpec.describe 'The Admin Merchant Index page', type: :feature do
       expect(page).to have_content(merchant_2.name)
       expect(page).to have_content(merchant_3.name)
     end
+
+    it 'has a link for each merchant name to the admin merchant show page' do
+      visit admin_merchants_path
+
+      expect(page).to have_link(merchant_1.name)
+      expect(page).to have_link(merchant_2.name)
+      expect(page).to have_link(merchant_3.name)
+
+      click_link(merchant_1.name)
+
+      expect(current_path).to eq(admin_merchants_path(merchant_1))
+    end
   end
 end
---
+++ spec/features/admins/merchants/show_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe 'The Admin Merchant Show page', type: :feature do
+  let!(:merchant_1) { create(:merchant) }
+
+
+end
