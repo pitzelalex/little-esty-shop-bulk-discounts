61c0c9ce77c938747ce63974750de2e6292b1a58
Kerynn Davis <kerynn.davis@gmail.com>
2023-01-03 19:27:45 -0800

    test: Add testing for update merchant form

--- spec/features/admins/merchants/show_spec.rb
+++ spec/features/admins/merchants/show_spec.rb
@@ -9,6 +9,37 @@ RSpec.describe 'The Admin Merchant Show page', type: :feature do
 
       expect(page).to have_content(merchant_1.name)
     end 
+
+    describe 'update Merchant form' do
+      it "has a link to update the merchant's information" do
+        visit admin_merchant_path(merchant_1)
+
+        click_link "Update Merchant"
+
+        expect(current_path).to eq edit_admin_merchant_path(merchant_1)
+        expect(page).to have_field('Name', with: "#{merchant_1.name}")
+      end
+
+      it 'redirects to the admin merchant show page once submitted and shows the updated merchant name' do
+        visit edit_admin_merchant_path(merchant_1)
+
+        fill_in 'Name', with: 'Charlie'
+        click_button 'Save'
+
+        expect(current_path).to eq admin_merchant_path(merchant_1)
+        expect(page).to have_content("Charlie")
+      end
+
+      it "shows a flash message if update was successful" do
+        visit edit_admin_merchant_path(merchant_1)
+
+        fill_in 'Name', with: 'Charlie'
+        click_button 'Save'
+
+        expect(page).to have_content("Charlie")
+        expect(page).to have_content("Merchant Information Successfully Updated")
+      end
+    end
   end 
 end
 
