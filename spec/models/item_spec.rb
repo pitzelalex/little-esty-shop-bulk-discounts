08e848c7e27bea2e4b324825768123064e91d1cb
Alex Pitzel <pitzelalex@gmail.com>
2023-01-05 01:09:02 -0500

    test: Update #invoice_item_by_invoice expectations

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -12,11 +12,18 @@ RSpec.describe Item, type: :model do
         it 'returns an items invoice items based on a specific invoice' do
           invoice_1 = create(:invoice)
           invoice_2 = create(:invoice)
-          item_1 = create(:item_with_invoice_item, invoice: invoice_1)
-          item_2 = create(:item_with_invoice_item, invoice: invoice_2)
+          # item_1 = create(:item_with_invoice_item, invoice: invoice_1)
+          # item_2 = create(:item_with_invoice_item, invoice: invoice_2)
+          item_1 = create(:item)
+          item_2 = create(:item)
+          ii_1 = create(:invoice_item, item: item_1, invoice: invoice_1)
+          ii_2 = create(:invoice_item, item: item_2, invoice: invoice_2)
 
           # require 'pry'; binding.pry
-          expect(item_1.invoice_items_by_invoice(invoice_1)).to eq(InvoiceItem.find(Invoice.first.id))
+          expect(item_1.invoice_item_by_invoice(invoice_1)).to eq(ii_1)
+          expect(item_1.invoice_item_by_invoice(invoice_1)).not_to eq(ii_2)
+          expect(item_2.invoice_item_by_invoice(invoice_2)).to eq(ii_2)
+          expect(item_2.invoice_item_by_invoice(invoice_2)).not_to eq(ii_1)
         end
       end
     end
