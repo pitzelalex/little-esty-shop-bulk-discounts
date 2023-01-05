0a6db69af5e7d82b02b10006915fc060f32640a7
Alex Pitzel <pitzelalex@gmail.com>
2023-01-05 12:43:34 -0500

    test: Create invoice_item model test for #total_revenue

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -4,5 +4,17 @@ RSpec.describe InvoiceItem, type: :model do
   describe 'relationships' do
     it {should belong_to :item}
     it {should belong_to :invoice}
+
+    describe 'instance methods' do
+      describe '#total_revenue' do
+        it 'returns the total revenue of the invoice item' do
+          ii_1 = create(:invoice_item, quantity: 4, unit_price: 20000)
+          ii_2 = create(:invoice_item, quantity: 10, unit_price: 12000)
+
+          expect(ii_1.total_revenue).to eq("$800.00")
+          expect(ii_2.total_revenue).to eq("$1200.00")
+        end
+      end
+    end
   end
 end
