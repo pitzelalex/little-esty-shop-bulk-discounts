9795020427416bccde1002891b3533ff2045fefe
Alex Pitzel <pitzelalex@gmail.com>
2023-01-05 01:00:11 -0500

    test: Create invoice model #invoice_items_by_invoice test

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -6,5 +6,19 @@ RSpec.describe Item, type: :model do
     it {should have_many :invoice_items}
     it {should have_many(:invoices).through(:invoice_items)}
     it {should have_many(:customers).through(:invoices)}
+
+    describe 'instance methods' do
+      describe '#invoice_items_by_invoice' do
+        it 'returns an items invoice items based on a specific invoice' do
+          invoice_1 = create(:invoice)
+          invoice_2 = create(:invoice)
+          item_1 = create(:item_with_invoice_item, invoice: invoice_1)
+          item_2 = create(:item_with_invoice_item, invoice: invoice_2)
+
+          # require 'pry'; binding.pry
+          expect(item_1.invoice_items_by_invoice(invoice_1)).to eq(InvoiceItem.find(Invoice.first.id))
+        end
+      end
+    end
   end
 end
