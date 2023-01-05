0c12af1f8e9282de00fa2c40e81ca787e7886cf5
Alex Pitzel <pitzelalex@gmail.com>
2023-01-05 11:54:48 -0500

    test: Update merchant_spec to test for new merchant associations

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -6,12 +6,14 @@ RSpec.describe Merchant, type: :model do
 
   describe "relationships" do
     it {should have_many :items}
-    it {should have_many(:invoices).through(:items)}
+    it {should have_many(:invoice_items).through(:items)}
+    it {should have_many(:invoices).through(:invoice_items)}
     it {should have_many(:customers).through(:invoices)}
+    it {should have_many(:transactions).through(:invoices)}
   end
 
   describe 'top_customers' do
-    it 'returns the top 5 customers with the most successful transactions' do
+    xit 'returns the top 5 customers with the most successful transactions' do
       expect(merchant_1.top_customers).to eq([])
     end
   end
