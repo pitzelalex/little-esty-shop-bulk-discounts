8b413f7ab44ba643e53dda521c4b4dc1a0eeb2b7
Alex Pitzel <pitzelalex@gmail.com>
2023-01-05 01:09:29 -0500

    feat: Create invoice_item_by_invoice in item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,8 @@ class Item < ApplicationRecord
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
   has_many :customers, through: :invoices
+
+  def invoice_item_by_invoice(invoice)
+    self.invoice_items.where(invoice_id: invoice.id).first
+  end
 end
