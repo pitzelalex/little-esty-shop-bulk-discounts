17d2dfb119798301c48c61ccc8fc84d26357d37c
Kerynn Davis <kerynn.davis@gmail.com>
2023-01-05 10:12:48 -0800

    feat: Add form to create a new merchant

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   get '/merchants/:id/dashboard', to: "merchants#show"
 
   namespace :admin do
-    resources :merchants, only: [:index, :show, :edit, :update]
+    resources :merchants
     resources :invoices, only: [:index, :show]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
