9fc50fc0b81073e9ac4a6a08c3fac8a16695791a
Kerynn Davis <kerynn.davis@gmail.com>
2023-01-06 10:48:59 -0800

    feat: Add sad path with flash message for merchant not created

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -30,8 +30,13 @@ class Admin::MerchantsController < ApplicationController
   end
 
   def create
-    @merchant = Merchant.create!(merchant_params)
-    redirect_to admin_merchants_path
+    @merchant = Merchant.new(merchant_params)
+    if @merchant.save
+      redirect_to admin_merchants_path
+    else 
+      redirect_to new_admin_merchant_path 
+      flash[:alert] = "Name field must not be empty. Please fill out and resubmit."
+    end 
   end
 
   private
