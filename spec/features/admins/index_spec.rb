f8fd9b39e0909aa068ab27d72aa2cd0b33ea7f17
Kerynn Davis <kerynn.davis@gmail.com>
2023-01-05 17:15:33 -0800

    feat: Add admin merchants index link

--- spec/features/admins/index_spec.rb
+++ spec/features/admins/index_spec.rb
@@ -7,5 +7,19 @@ RSpec.describe 'Admin Dashboard', type: :feature do
 
       expect(page).to have_content("Admin Dashboard")
     end 
+
+    it 'has a link to the admin merchants index page' do 
+      visit admin_index_path 
+
+      expect(page).to have_link "Admin Merchants Index"
+
+      click_link("Admin Merchants Index")
+
+      expect(current_path).to eq admin_merchants_path
+    end
+
+    xit 'has a link to the admin invoices index page' do 
+
+    end 
   end
 end
