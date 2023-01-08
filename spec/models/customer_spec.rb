1eb7fe42bab1ac1428f5096f20095728fedfff90
Alex Pitzel <pitzelalex@gmail.com>
2023-01-08 02:09:22 -0500

    test: Create customer model #full_name test

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -4,5 +4,15 @@ RSpec.describe Customer, type: :model do
   describe 'relationships' do
     it {should have_many :invoices}
     it {should have_many :transactions}
+
+    define 'instance methods' do
+      define '#full_name' do
+        it 'returns the customers full name' do
+          customer = create(:customer, first_name: 'Alex', last_name: 'Pitzel')
+
+          expect(customer.full_name).to eq('Alex Pitzel')
+        end
+      end
+    end
   end
 end
