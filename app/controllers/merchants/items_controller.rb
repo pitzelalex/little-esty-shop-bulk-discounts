a1c38f1476b453c6cfb9d7e23a32cc205d825f62
Thomas Casady <tscasady@gmail.com>
2023-01-08 13:09:35 -0600

    fix: Fix incorrect params used in update item. Refactored enable/disable partial.

--- app/controllers/merchants/items_controller.rb
+++ app/controllers/merchants/items_controller.rb
@@ -33,7 +33,7 @@ class Merchants::ItemsController < ApplicationController
     @merchant = Merchant.find(params[:merchant_id])
     @item = Item.find(params[:id])
     if params[:status] != nil
-      @item.update!(merchant_item_params)
+      @item.update!(update_params)
       redirect_to merchant_items_path(@merchant)
     elsif @item.update(merchant_item_params)
       flash[:alert] = "Item Information Successfully Updated"
@@ -48,5 +48,9 @@ class Merchants::ItemsController < ApplicationController
 
   def merchant_item_params
     params.require(:item).permit(:name, :description, :unit_price, :status)
+  end
+
+  def update_params
+    params.permit(:status)
   end
 end
