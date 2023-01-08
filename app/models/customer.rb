ec40e86998befe21cf7278e3ccd75b9c6c49b673
Alex Pitzel <pitzelalex@gmail.com>
2023-01-08 02:20:09 -0500

    feat: Create customer #full_name

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,4 +2,8 @@ class Customer < ApplicationRecord
   has_many :invoices 
   has_many :transactions, through: :invoices
   has_many :merchants, through: :invoices
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
 end
