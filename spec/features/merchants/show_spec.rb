abd44b3c777f2ad2fe8943db70e33271e5d1555a
Alex Pitzel <pitzelalex@gmail.com>
2023-01-08 02:22:08 -0500

    test: Create merchant dashboard top customers

--- spec/features/merchants/show_spec.rb
+++ spec/features/merchants/show_spec.rb
@@ -35,10 +35,25 @@ RSpec.describe 'it shows the merchant dashboard page', type: :feature do
     end
 
     describe 'Favorite Customers' do
-      xit 'shows the names of the top 5 customers by number of successful transactions' do
+      it 'shows the names of the top 5 customers by number of successful transactions' do
+        cus1 = create(:customer_with_success_trans, merchant: mer1, inv_count: 3)
+        cus2 = create(:customer_with_success_trans, merchant: mer1, inv_count: 4)
+        cus3 = create(:customer_with_success_trans, merchant: mer1, inv_count: 1) # cus 3 not in top
+        cus4 = create(:customer_with_success_trans, merchant: mer1, inv_count: 2)
+        cus5 = create(:customer_with_success_trans, merchant: mer1, inv_count: 5)
+        cus6 = create(:customer_with_success_trans, merchant: mer1, inv_count: 6)
+        cus2_invoices = 5.times { create(:invoice_with_transactions, merchant: mer2, customer: cus2)}
+        cus2_unsuccesful_invoices = 5.times { create(:invoice_with_transactions, merchant: mer1, customer: cus2, invoice_has_success: false)}
+
         visit "/merchants/#{merchant_1.id}/dashboard"
 
-        expect(page).to have_content ""
+        within "#top_customers" do
+          expect(page).to have_content 'Top Customers'
+          expect(cus6.full_name).to appear_before cus5.full_name
+          expect(cus5.full_name).to appear_before cus2.full_name
+          expect(cus2.full_name).to appear_before cus1.full_name
+          expect(cus1.full_name).to appear_before cus4.full_name
+        end
       end
 
       xit 'shows the number of successful transactions next to each customers name' do
