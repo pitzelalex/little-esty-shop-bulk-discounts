d9a76c72e18776ee3dfe853b444fb26a055dfc7b
Kerynn Davis <kerynn.davis@gmail.com>
2023-01-08 10:17:54 -0800

    set up testing for top customers on admin merchant dashboard

--- spec/features/admins/index_spec.rb
+++ spec/features/admins/index_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe 'Admin Dashboard', type: :feature do 
+
+  let!(:merchant_1) { create(:merchant) }
+  let!(:merchant_2) { create(:merchant) }
+  let!(:merchant_3) { create(:enabled_merchant) }
+
   describe 'when the user visits the admin dashboard' do 
     it 'shows a header indicating the user is on the admin dashboard' do 
       visit admin_index_path 
@@ -26,6 +31,28 @@ RSpec.describe 'Admin Dashboard', type: :feature do
       click_link("Admin Invoices Index")
       
       expect(current_path).to eq admin_invoices_path
-    end 
+    end
+
+    describe 'top customers' do 
+      it 'displays the top 5 customers by successful transaction' do
+        customer_1 = create(:customer_with_success_trans, merchant: merchant_1, inv_count: 3)
+        customer_2 = create(:customer_with_success_trans, merchant: merchant_1, inv_count: 8)
+        customer_3 = create(:customer_with_success_trans, merchant: merchant_2, inv_count: 5)
+        customer_4 = create(:customer_with_success_trans, merchant: merchant_2, inv_count: 2)
+        customer_5 = create(:customer_with_success_trans, merchant: merchant_1, inv_count: 7)
+        customer_6 = create(:customer_with_success_trans, merchant: merchant_3, inv_count: 4)
+        # cust5_unsuccessful_invoices = 3.times { create!(:invoice_with_transactions, merchant: merchant_1, customer: customer_5, invoice_has_success: fail)}
+
+        visit admin_index_path 
+
+        within '#top_customers' do 
+          expect(customer_2.full_name).to appear_before(customer_5.full_name)
+        end
+      end
+
+      xit 'shows the number of successful transactions next to each of the top 5 customers' do 
+
+      end
+    end
   end
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -5,4 +5,21 @@ RSpec.describe Customer, type: :model do
     it {should have_many :invoices}
     it {should have_many :transactions}
   end
+
+  describe 'top_5_customers' do
+    it 'determines the top 5 customers with successful transactions' do 
+      merchant_1 = create(:merchant)
+      merchant_2 = create(:merchant)
+      merchant_3 = create(:enabled_merchant)
+    
+      customer_1 = create(:customer_with_success_trans, merchant: merchant_1, inv_count: 3)
+      customer_2 = create(:customer_with_success_trans, merchant: merchant_1, inv_count: 8)
+      customer_3 = create(:customer_with_success_trans, merchant: merchant_2, inv_count: 5)
+      customer_4 = create(:customer_with_success_trans, merchant: merchant_2, inv_count: 2)
+      customer_5 = create(:customer_with_success_trans, merchant: merchant_1, inv_count: 7)
+      customer_6 = create(:customer_with_success_trans, merchant: merchant_3, inv_count: 4)
+
+      expect(Customer.top_5_customers).to eq([customer_2, customer_5, customer_3, customer_6, customer_1])
+    end
+  end 
 end
