a9d968e97ce448f7107842b1f6ee0fce9ff5c6ac
Kerynn Davis <kerynn.davis@gmail.com>
2023-01-08 11:36:25 -0800

    add testing for unsuccessful invoices

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -18,6 +18,8 @@ RSpec.describe Customer, type: :model do
       customer_4 = create(:customer_with_success_trans, merchant: merchant_2, inv_count: 2)
       customer_5 = create(:customer_with_success_trans, merchant: merchant_1, inv_count: 7)
       customer_6 = create(:customer_with_success_trans, merchant: merchant_3, inv_count: 4)
+      cust5_unsuccessful_invoices = 3.times { create(:invoice_with_transactions, merchant: merchant_1, customer: customer_5, invoice_has_success: false)}
+      cust6_unsuccessful_invoices = 3.times { create(:invoice_with_transactions, merchant: merchant_3, customer: customer_6, invoice_has_success: false)}
 
       expect(Customer.top_5_customers).to eq([customer_2, customer_5, customer_3, customer_6, customer_1])
     end
