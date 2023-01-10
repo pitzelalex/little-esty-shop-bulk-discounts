99302efa1b52012dea05bd9256442eb71be46e4c
Alex Pitzel <pitzelalex@gmail.com>
2023-01-10 16:15:52 -0500

    refactor: Update merchant invoices show page to match wireframe

--- spec/features/merchants/invoices/show_spec.rb
+++ spec/features/merchants/invoices/show_spec.rb
@@ -10,10 +10,10 @@ RSpec.describe 'The merchant invoice show page', type: :feature do
       merchant_1.invoices.each do |invoice|
         visit merchant_invoice_path(merchant_1, invoice)
 
-        expect(page).to have_content("Id: #{invoice.id}")
+        expect(page).to have_content("Invoice ##{invoice.id}")
         expect(page).to have_content("Status: #{invoice.status}")
         expect(page).to have_content("Created at: #{invoice.created_at.strftime("%A, %B %-d, %Y")}")
-        expect(page).to have_content("Customer: #{invoice.customer.first_name} #{invoice.customer.last_name}")
+        expect(page).to have_content("#{invoice.customer.first_name} #{invoice.customer.last_name}")
       end
     end
 
