bfb1d5e7dccc492a4470e71d37f19a311b7c034f
Thomas Casady <tscasady@gmail.com>
2023-01-10 17:18:55 -0600

    feat: Add GithubDecorator.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,31 @@
 module ApplicationHelper
+  class GithubDecorator
+    def initialize(githubrepo)
+      @git = githubrepo
+    end
+
+    def usernames
+      @git.usernames.join(', ')
+    end
+
+    def commits
+      hash_to_display(@git.collab_commits)
+    end
+
+    def pull_requests
+      hash_to_display(@git.pull_requests)
+    end
+
+    def repo_name
+      @git.repo_name
+    end
+
+    private
+
+    def hash_to_display(hash)
+      hash.map do |key, value|
+        "#{key}: #{value}"
+      end.join(', ')
+    end
+  end
 end
