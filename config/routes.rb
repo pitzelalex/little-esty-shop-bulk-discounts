d444e5963adbb5bfada61b4de5c92806dc28034b
Kerynn Davis <kerynn.davis@gmail.com>
2023-01-10 07:50:30 -0800

    update all routes to advance minus merchant dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  resources :customers
-  resources :merchants, only: [] do
+
+  resources :merchants do
     resources :items, controller: "merchants/items"
     resources :invoices, only: [:index, :show], controller: 'merchants/invoices'
   end
+  
   resources :invoice_items, only: :update
 
   get '/merchants/:id/dashboard', to: "merchants#show"
@@ -11,7 +12,7 @@ Rails.application.routes.draw do
   resources :admin, only: [:index]
 
   namespace :admin do
-    resources :merchants
+    resources :merchants, except: [:destroy]
     resources :invoices, only: [:index, :show]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
