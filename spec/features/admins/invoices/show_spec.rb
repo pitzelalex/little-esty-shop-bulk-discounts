7354059c9fccefb11305645fbe8d4636b019ecd0
Alex Pitzel <pitzelalex@gmail.com>
2023-01-11 14:07:12 -0500

    refactor: Update admin invoice show page to better match wireframe

--- spec/features/admins/invoices/show_spec.rb
+++ spec/features/admins/invoices/show_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+include ActionView::Helpers::NumberHelper
 
 RSpec.describe 'Admin Invoice Show Page' do
     before(:each) do
@@ -11,7 +12,7 @@ RSpec.describe 'Admin Invoice Show Page' do
             visit admin_invoice_path(@invoice_1)
 
             expect(page).to have_content("Invoice ##{@invoice_1.id}")
-            expect(page).to have_content("Status: #{@invoice_1.status}")
+            expect(page).to have_content("Status:")
             expect(page).to have_content("Created at: #{@invoice_1.created_at.strftime("%A, %B%e, %Y")}")
             expect(page).to have_content("Customer: #{@invoice_1.customer.first_name} #{@invoice_1.customer.last_name}")
         end
@@ -19,28 +20,30 @@ RSpec.describe 'Admin Invoice Show Page' do
         it 'has all the items on the invoice and their properties' do
             visit admin_invoice_path(@invoice_1)
 
-            expect(page).to have_content(@invoice_1.items.first.name)
-            expect(page).to have_content(@invoice_1.items.last.name)
-
-            expect(page).to have_content(@invoice_1.items.first.invoice_item_by_invoice(@invoice_1).quantity)
-            expect(page).to have_content(@invoice_1.items.last.invoice_item_by_invoice(@invoice_1).quantity)
-            expect(page).to have_content(@invoice_1.items.first.invoice_item_by_invoice(@invoice_1).unit_price)
-            expect(page).to have_content(@invoice_1.items.last.invoice_item_by_invoice(@invoice_1).unit_price)
-            expect(page).to have_content(@invoice_1.items.first.invoice_item_by_invoice(@invoice_1).status)
-            expect(page).to have_content(@invoice_1.items.last.invoice_item_by_invoice(@invoice_1).status)
+            within "#items" do
+                expect(page).to have_content('Items on this Invoice:')
+                @invoice_1.items.each do |item|
+                    within "#item-#{item.id}" do
+                        expect(page).to have_content("#{item.name}")
+                        expect(page).to have_content("#{item.invoice_items.where(invoice_id: @invoice_1.id).first.quantity}")
+                        expect(page).to have_content("#{number_to_currency((item.invoice_items.where(invoice_id: @invoice_1.id).first.unit_price)/100.00)}")
+                        expect(page).to have_content("#{item.invoice_items.where(invoice_id: @invoice_1.id).first.status}")
+                    end
+                end
+            end
         end
 
         it 'displays the total revenue generated from this invoice' do
             visit admin_invoice_path(@invoice_1)
 
-            expect(page).to have_content("Total Revenue: #{@invoice_1.total_revenue}")
+            expect(page).to have_content("Total Revenue: #{number_to_currency(@invoice_1.total_revenue / 100)}")
         end
 
         it 'has invoice status as a select field and the current status is selected' do
             visit admin_invoice_path(@invoice_1)
 
             expect(page).to have_select('invoice[status]', selected: "#{@invoice_1.status}")
-            expect(page).to have_select('invoice[status]', options: ['in progress', 'cancelled', 'completed'])
+            expect(page).to have_select('invoice[status]', options: ['','in progress', 'cancelled', 'completed'])
         end
 
         it "has a button to 'Update Invoice Status' which updates the invoice status when clicked" do
