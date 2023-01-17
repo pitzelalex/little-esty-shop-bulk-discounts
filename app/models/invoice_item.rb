0ceddf68152c59675bd25676d9c9749d8783fcea
Alex Pitzel <pitzelalex@gmail.com>
2023-01-17 13:52:58 -0500

    cleanup: Remove old methods that were commented out

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -7,19 +7,6 @@ class InvoiceItem < ApplicationRecord
   validates_presence_of :status
   enum status: ['pending', 'packaged', 'shipped']
 
-  # def revenue
-  #   InvoiceItem.where(id: self.id).sum('quantity*unit_price')
-  # end
-
-  # def discounted_revenue
-  #   if self.bulk_discounts.where('threshold <= ?', self.quantity).count == 0
-  #     revenue
-  #   else
-  #     discount = self.bulk_discounts.where('threshold <= ?', self.quantity).order(threshold: :desc).first.discount
-  #     revenue * discount
-  #   end
-  # end
-
   def discount
     self.bulk_discounts.where('threshold <= ?', self.quantity).order(threshold: :desc).first.try(:discount)
   end
--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -12,32 +12,6 @@ RSpec.describe InvoiceItem, type: :model do
   end
 
   describe 'instance methods' do
-    # describe '#revenue' do
-    #   it 'returns the total revenue of the invoice item' do
-    #     ii_1 = create(:invoice_item, quantity: 4, unit_price: 20000)
-    #     ii_2 = create(:invoice_item, quantity: 10, unit_price: 12000)
-
-    #     expect(ii_1.revenue).to eq(80000)
-    #     expect(ii_2.revenue).to eq(120000)
-    #   end
-    # end
-
-    # describe '#discounted_revenue' do
-    #   it 'returns the total discounted revenue of the invoice item' do
-    #     merchant = create(:merchant_with_items, num: 3)
-    #     invoice = create(:invoice)
-    #     ii1 = create(:invoice_item, item: merchant.items[0], invoice: invoice, quantity: 20, unit_price: 100000) # 1_600_000
-    #     ii2 = create(:invoice_item, item: merchant.items[1], invoice: invoice, quantity: 10, unit_price: 100000) # 900_000
-    #     ii3 = create(:invoice_item, item: merchant.items[2], invoice: invoice, quantity: 5, unit_price: 100000) # 500_000
-    #     bd1 = create(:bulk_discount, threshold: 10, discount: 0.90, merchant: merchant)
-    #     bd2 = create(:bulk_discount, threshold: 20, discount: 0.80, merchant: merchant)
-
-    #     expect(ii1.discounted_revenue).to eq(1600000)
-    #     expect(ii2.discounted_revenue).to eq(900000)
-    #     expect(ii3.discounted_revenue).to eq(500000)
-    #   end
-    # end
-
     describe '#discount' do
       it 'returns the decimal value of the appropriate discount that applies to an invoice item' do
         merchant = create(:merchant_with_items, num: 3)
